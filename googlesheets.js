2f8de1b2c95fb0a4ecdcf6de60ca83538fa3ba74
Daniela <nanysmanila@hotmail.com>
2024-08-27 11:30:53 -0500

    agrego registro fecha y hora mensaje

--- googlesheets.js
+++ googlesheets.js
@@ -9,3 +9,26 @@ form.addEventListener('submit', e =>{
     .then(()=> { window.location.reload(); })
     .catch(error => console.error('Error!', error.message))
 })
+
+// // Obtener la fecha y hora actual
+// const fechaHora = new Date();
+// // Formatear la fecha y hora como quieras (ejemplo: YYYY-MM-DD HH:mm:ss)
+// const fechaHoraFormateada = fechaHora.toISOString();
+
+// // Asignar la fecha y hora formateada al campo oculto
+// document.getElementById('fechaHora').value = fechaHoraFormateada;
+
+const fechaHora = new Date();
+
+const año = fechaHora.getFullYear();
+const mes = (fechaHora.getMonth() + 1).toString().padStart(2, '0');
+const dia = fechaHora.getDate().toString().padStart(2, '0');
+const fechaFormateada = `${año}-${mes}-${dia}`;
+document.getElementById('fecha').value = fechaFormateada;
+
+const horas = fechaHora.getHours().toString().padStart(2, '0');
+const minutos = fechaHora.getMinutes().toString().padStart(2, '0');
+const segundos = fechaHora.getSeconds().toString().padStart(2, '0');
+const   
+ horaFormateada = `${horas}:${minutos}:${segundos}`;
+document.getElementById('hora').value = horaFormateada;
