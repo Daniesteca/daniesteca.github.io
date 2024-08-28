a7366c4e191ecb56d379cb0564ba9b36925f715f
Daniela <nanysmanila@hotmail.com>
2024-08-28 11:13:24 -0500

    Ajustes skill

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ function efectoHabilidades(){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("htmlcss");
         habilidades[1].classList.add("javascript");
-        habilidades[2].classList.add("python");
-        habilidades[3].classList.add("mysql");
+        habilidades[2].classList.add("mysql");
+        habilidades[3].classList.add("python");
         habilidades[4].classList.add("comunicacion");
         habilidades[5].classList.add("trabajoequipo");
         habilidades[6].classList.add("creatividad");
